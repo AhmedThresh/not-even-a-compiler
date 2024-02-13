685fcf3fcbabc45f8b9ecb7ef70b53eb73bf9551
AhmedGrati <ahmedgrati1999@gmail.com>
2024-02-13 23:32:42 +0100

    feat: support builtins functions for arrays
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

--- pkg/eval/buitlins.go
+++ pkg/eval/buitlins.go
@@ -12,9 +12,81 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) < 1 {
+					return NULL
+				}
+				return arg.Elements[0]
+			default:
+				return newError("argument to `first` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) < 1 {
+					return NULL
+				}
+				return arg.Elements[len(arg.Elements)-1]
+			default:
+				return newError("argument to `last` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) < 1 {
+					return NULL
+				}
+				return &object.Array{Elements: arg.Elements[1:len(arg.Elements)]}
+			default:
+				return newError("argument to `rest` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			arr.Elements = append(arr.Elements, args[1])
+			return arr
 		},
 	},
 }
